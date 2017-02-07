5b2a9ee930b059bbc098e843b9ab9b77c3126574
Tristan\Tristanus <tristan_98@hotmail.nl>
2017-02-07 14:05:42 +0100

    added table and first letter

--- js/lingo.js
+++ js/lingo.js
@@ -1,7 +1,11 @@
+var firstLetter;
+
 function getWord(){
 	var randomNumber = (Math.floor(Math.random() * words.length));
 	var randomWord = words[randomNumber];
 	console.log(randomWord);
+	firstLetter = randomWord.substring(0, 1);
+	console.log(firstLetter);
 }
 
 getWord();
