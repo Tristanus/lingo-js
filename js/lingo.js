4e234aa39c04edaa175d01c61e24df9875d4732f
Tristan\Tristanus <tristan_98@hotmail.nl>
2017-02-13 15:40:01 +0100

    almost done with check function

--- js/lingo.js
+++ js/lingo.js
@@ -1,11 +1,22 @@
-var firstLetter;
+var guesses = 5;
 
 function getWord(){
 	var randomNumber = (Math.floor(Math.random() * words.length));
 	var randomWord = words[randomNumber];
-	console.log(randomWord);
-	firstLetter = randomWord.substring(0, 1);
-	console.log(firstLetter);
+	return randomWord;
 }
 
-getWord();
+var currentRow = 1;
+var row = 1;
+var letterAnswer = getWord().split();
+
+function check(){
+	var currentLetter = 1;
+	var word = document.getElementById('wordGuess').value;
+	var letterInput = word.split("");
+	for(i = 0; i < 4; i++){
+		document.getElementById(currentRow + "." + currentLetter).innerHTML = letterInput[i];
+		if(letterInput)
+	}
+}
+
